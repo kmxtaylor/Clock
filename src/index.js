b98fdfdb7880928ad16724d17465fbfcdffd34ce
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-24 13:49:44 -0700

    init main screen groundwork
    load fonts, set up splashscreen, add SafeAreaView & padding subcontainer; will experiment to find exact fonts needed later

--- src/index.js
+++ src/index.js
@@ -1,7 +1,69 @@
-import ClockScreen from "./screens/ClockScreen";
+import { useCallback } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  View,
+} from 'react-native';
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import * as SplashScreen from 'expo-splash-screen';
+
+import ClockScreen from 'screens/ClockScreen';
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-  return <ClockScreen />;
-}
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar barStyle='light-content' />
+      <SafeAreaView
+        style={styles.container}
+      >
+        <View
+          onLayout={onLayoutRootView}
+          style={styles.subContainer}
+          testID='app-screen'
+        >
+          <ClockScreen />
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  subContainer: {
+    flex: 1,
+    // backgroundColor: '',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    justifyContent: 'space-between',
+  },
+});
 
 export default App;
