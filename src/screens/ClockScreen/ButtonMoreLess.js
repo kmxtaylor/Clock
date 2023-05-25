fb43d68ca45f9f3306427d88b58ac4212140df73
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-24 21:04:41 -0700

    add icon to more/less btn
    initial design of btn done

--- src/screens/ClockScreen/ButtonMoreLess.js
+++ src/screens/ClockScreen/ButtonMoreLess.js
@@ -1,28 +1,47 @@
 import { StyleSheet, Pressable } from 'react-native';
 
 import Text from 'components/Text';
+import IconArrowUp from 'components/svgs/IconArrowUp';
+
+const IconArrow = ({ showingMore }) => {
+  if (showingMore) {
+    return (
+      <IconArrowUp /> // arrow up
+    );
+  }
+  else {
+    return (
+      <IconArrowUp style={{transform: [{ rotate: '180deg' }]}} /> // arrow down
+    );
+  }
+};
 
 // expansion should probably be handled in ClockScreen b/c affects ExpandedInfo
-const ButtonMoreLess = ({ isExpanded, setIsExpanded, style, ...rest }) => {
+const ButtonMoreLess = ({ showingMore, setShowingMore, style, ...rest }) => {
   return (
     <Pressable
       style={[styles.btn, style]}
-      onPress={() => setIsExpanded(!isExpanded)}
+      onPress={() => setShowingMore(!showingMore)}
       {...rest}
     >
       <Text style={styles.text}>
-        {isExpanded ? 'Less' : 'More'}
+        {showingMore ? 'Less' : 'More'}
       </Text>
+      <IconArrow showingMore={showingMore} />
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   btn: {
-    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
     width: 140,
     backgroundColor: 'white',
     borderRadius: 25,
+    padding: 5,
     // justifyContent: 'center',
     // alignItems: 'flex-start',
     // paddingVertical: 10,
@@ -39,7 +58,8 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     color: 'grey',
     textTransform: 'uppercase',
-    letterSpacing: 2,
+    letterSpacing: 4,
+    marginLeft: 15,
   },
 });
 
