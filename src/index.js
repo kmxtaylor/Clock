8ba28c82ed74bd1737de662f93d7674d50216d01
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-28 21:09:12 -0700

    reset font to default
    comment out all code explicitly loading Roboto font

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-import { useCallback } from 'react';
+// import { useCallback } from 'react';
 
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_700Bold
-} from '@expo-google-fonts/roboto';
-import * as SplashScreen from 'expo-splash-screen';
+// import {
+//   useFonts,
+//   Roboto_400Regular,
+//   Roboto_500Medium,
+//   Roboto_700Bold
+// } from '@expo-google-fonts/roboto';
+// import * as SplashScreen from 'expo-splash-screen';
 
 import { ModeProvider } from 'contexts/Mode';
 import ClockScreen from 'screens/ClockScreen';
 import Main from 'layouts/Main';
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold
-  });
+  // const [fontsLoaded] = useFonts({
+  //   Roboto_400Regular,
+  //   Roboto_500Medium,
+  //   Roboto_700Bold
+  // });
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
 
-  if (!fontsLoaded) {
-    return null; // all hooks calls must be before return
-  }
+  // if (!fontsLoaded) {
+  //   return null; // all hooks calls must be before return
+  // }
 
   return (
     <ModeProvider>
       <Main
-        onLayout={onLayoutRootView}
+        // onLayout={onLayoutRootView}
       >
         <ClockScreen />
       </Main>
